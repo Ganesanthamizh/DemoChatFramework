0a93edd18ce72703cd60ecb15037b3f1d029a207
Ganesanthamizh <erganeshtamizh@gmail.com>
2020-05-20 12:58:41 +0530

    Initial commit with cocoapod

---
+++ Example/Pods/Target Support Files/DemoChatFramework/DemoChatFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DemoChatFramework : NSObject
+@end
+@implementation PodsDummy_DemoChatFramework
+@end
---
+++ Example/Pods/Target Support Files/Pods-DemoChatFramework_Tests/Pods-DemoChatFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoChatFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DemoChatFramework_Tests
+@end
